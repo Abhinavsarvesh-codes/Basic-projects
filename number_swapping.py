55c891ea1be85e3e5238b2ffb23e3d61f30d77a8
Abhinavsarvesh-codes <abhinavsarveshmn@gmail.com>
2026-07-06 10:10:46 +0530

    projcet

---
+++ number_swapping.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=a+b
+print("Before swapping : a = ",a,"b =",b)
+print(f"After awaping : a = {c-a} , b = {c-b}")
---
+++ positive_check_in_python.py
@@ -0,0 +1,7 @@
+num=int(input("Enter the number :"))
+if num>0:
+    print("It's positive")
+elif num<0:
+    print("It's negative")
+else:
+    print("It's zero")
