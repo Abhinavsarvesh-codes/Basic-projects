dbd75e45e684824fdce978092f9d306db820ccf4
Abhinavsarvesh-codes <abhinavsarveshmn@gmail.com>
2026-07-29 11:46:46 +0530

    project

---
+++ count_ones.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int countDigitOne(int n) {
+    int count=0;
+      for(int i=1;i<=n;i++){
+        int temp = i;
+        while(temp>0){
+            if(temp%10==1){
+                count++;
+            }
+            temp/=10;
+        }
+      }
+        return count;
+    }
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", countDigitOne(n));
+    return 0;
+}
